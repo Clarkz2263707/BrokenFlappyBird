15fc67c80cb91611e523b66754464b84ebd0302e
Clarkz2263707 <zachary.clark4@g.austincc.edu>
2025-08-04 00:50:21 -0500

    added private Vector2 initial position to initialize the position upon the game starting. added a float offset to properly lay out the warping/respawning of the ground so it isnt just dissapearing and reappearing.

--- Assets/BrokenFlappyBird-master/Assets/Script/GroundScroll.cs
+++ Assets/BrokenFlappyBird-master/Assets/Script/GroundScroll.cs
@@ -6,13 +6,21 @@ public class GroundScroll : MonoBehaviour
     public float resetPositionX; 
     public float startPositionX; 
 
+    private Vector2 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
     void Update()
     {
         transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
 
         if (transform.position.x <= resetPositionX)
         {
-            Vector2 newPos = new Vector2(startPositionX, transform.position.y);
+            float offset = transform.position.x - resetPositionX;
+            Vector2 newPos = new Vector2(startPositionX + offset, transform.position.y);
             transform.position = newPos;
         }
     }
